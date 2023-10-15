368e8da3db408bd9b8740baabef259d10899361b
tykman <nizom.ra@inbox.ru>
2023-10-15 07:50:47 +0500

    начальный коммит д/з по теме: Тестирование

---
+++ src/main/java/com/example/calculatorTest/CalculatorTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatorTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorTestApplication.class, args);
+	}
+
+}
